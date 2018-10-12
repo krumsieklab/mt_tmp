93a3a5135253089c02e5b7cfaa5f7f81c8520985
Unknown <krumsiek@gmx.net>
2018-10-11 23:02:17 -0400

    added dependency sources to examples

--- mtExamples.R
+++ mtExamples.R
@@ -1,4 +1,13 @@
 
+
+#### source all files except myself... ugly but works for now ----
+l=list.files(path=codes.makepath("packages/metabotools"),pattern="*.R",full.names=T)
+l=l[!grepl('mtExamples.R',l)]
+lapply(l, function(x){source(x,echo=F,verbose=F)})
+
+source(codes.makepath('R/data/parseMetabolonFile.R'))
+
+
 #### ----
 D = parseMetabolonFile(codes.makepath("packages/metabotools/sampledata.xlsx"),"OrigScale",sumexp = T) %>% 
   mtFilter(metMax=0.2) %>% mtFilter(sampleMax=0.1) %>% mtQuotNorm()
