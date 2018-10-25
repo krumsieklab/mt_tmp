5107e2afa7094f2c608cdaf0444d7745d33f31a4
zsanoj <sanoj@zierer.uk>
2018-10-25 16:44:53 -0400

    add foldchanges and volcano plot

---
+++ mt_plots_volcano.R
@@ -0,0 +1,81 @@
+################################################################################
+## HEATMAP
+################################################################################
+#' mt_plot_volcano
+#'
+#' create volcano plot
+#'
+#' @author Jonas Zierer
+#' @import SummrizedExperiment
+#' @import ggplot2
+#' @importFrom dplyr %>% mutate gather left_join filter arrange
+#' @importFrom rlang enquo !!
+#' @param D SummarizedExperiment object
+#' @param x what value shall be plotted on x (default: fc)
+#' @param statname name of the statistics obkect to plot
+#' @param metab_filter if given, filter will be applied to data and remaining varaibles will be labelled in plot
+#' @param ... further parameters forwarded to ggplot::aes
+#' @return SummarizedExperiment with volcano plot in metadata(D)$results
+#' @export mt_plot_volcano
+mt_plots_volcano <- function(D,
+                             x = fc,
+                             statname,
+                             metab_filter = p.value < 0.05,
+                             ...){
+    x <- enquo(x)
+    
+    ## check input
+    stopifnot("SummarizedExperiment" %in% class(D))
+    if(missing(statname))
+        stop("statname must be given for volcanoplot")
+
+    ## rowData
+    rd <- rowData(D) %>%
+        as.data.frame() %>%
+        mutate(var = rownames(D))
+    
+    ## stat
+    data_plot <- mti_get_stat_by_name(D, statname) %>%
+        inner_join(rd, by = "var") %>%
+        mutate(xxx = !!x)
+
+    ## SCALE -log10
+    reverselog_trans <- function (base = exp(1)){
+        trans <- function(x) -log(x, base)
+        inv <- function(x) base^(-x)
+        scales::trans_new(paste0("reverselog-", format(base)), trans, inv, 
+                          scales::log_breaks(base = base),
+                          domain = c(1e-100, Inf))
+    }
+    ## CREATE PLOT
+    p <- data_plot %>%
+        ## do plot
+        ggplot(aes(x = xxx, y = p.value)) +
+        geom_point(aes(...)) +
+        scale_y_continuous(trans = reverselog_trans(10),
+                           breaks = scales::trans_breaks("log10", function(x) 10^x),
+                           labels = scales::trans_format("log10", scales::math_format(10^.x))) +
+        labs(x = NULL, y = "p-value") +
+        ggtitle(statname)
+
+    ## ADD METABOLITE LABELS
+    if(!missing(metab_filter)){
+        logmsg("add label")
+        metab_filter_q <- enquo(metab_filter)
+        data_annotate <- data_plot %>%
+            filter(!!metab_filter_q)
+        p <- p + ggrepel::geom_text_repel(data = data_annotate,
+                           aes(label = name))
+    }
+
+    ## add status information & plot
+    funargs <- mti_funargs()
+    metadata(D)$results %<>% 
+                  mti_generate_result(
+                      funargs = funargs,
+                      logtxt = sprintf("volcano plot, aes: %s", mti_dots_to_str(...)),
+                      output = list(p)
+                  )
+    ## return
+    D
+}
---
+++ mt_post_addFC.R
@@ -0,0 +1,107 @@
+################################################################################
+## ADD FOLDCHANGES TO RESULTS TABLE
+################################################################################
+#' mt_post_addFC
+#'
+#' add metabolite foldchanges to results table
+#'
+#' @author Jonas Zierer
+#' @import SummarizedExperiment
+#' @param D SummarizedExperiment object
+#' @param statname name of the statistical comparison
+#' @param correct_confounder confounders to adjust for before calculating FC
+#' @return SummarizedExperiment modifies stats object
+#' @export mt_post_addFC
+mt_post_addFC <- function(D,
+                          statname,
+                          correct_confounder,
+                          combine = mean,
+                          ...){
+
+    ## FOLDCHANGE FUNCTION (CONSIDER PREVIOUS LOG)
+    is_logscale <- metadata(D)$results %>% map_lgl(~"log" %in% .x$fun) %>% any()
+    if(!is_logscale)
+        stop("fold-changes can only be calculated for log-scale data")
+
+    ## stat
+    if(missing(statname))
+        stop("statname must be given")
+    ## FIND ENTRY
+    stat_id <- metadata(D)$results %>%
+                         map_lgl(~"stats" %in% .x$fun && .x$output$name == statname) %>%
+                         which()
+    if(length(stat_id) == 0)
+        stop("stat element with name ", statname, " does not exist")
+    if(length(stat_id)  > 1)
+        stop("there are multiple stat elements with name ", statname)
+
+    ## GET OUTCOME
+    model <- metadata(D)$results[[ stat_id ]] %>%
+                       .$output %>%
+                       ## get first model in list (doesn't matter which)
+                       .$lstobj %>%
+                       .[[1]] %>%
+                       ## get model matrix
+                       .$model %>%
+                       ## remove metabolites
+                       select(-one_of(intersect(names(.), rownames(D)))) %>%
+                       ## select first phenotype = outcome of interest
+                       .[,1, drop = F]
+    outcome    <- colnames(model)[[1]]
+
+    ## CORRECT FOR CONFOUNDER
+    formula    <- metadata(D)$results[[ stat_id ]]$output$formula
+    confounder <- update.formula(formula, str_c(".~.-", outcome))
+    confounder_terms <- attr(terms(confounder), "term.labels")
+    if(length(confounder_terms) > 0){
+        logmsg(glue::glue("correcting for {confounder}"))
+        D <- mti_correctConfounder(D, confounder)
+    }
+
+    ## EXTRACT DATA
+    d_fc <- mti_format_se_samplewise(D) %>%
+        ## use only levels that were used in stat
+        inner_join(model, by = outcome)
+
+    ## CHECK TYPE OF OUTCOME
+    if(class(d_fc[[ outcome ]]) %!in% c("factor", "character"))
+        stop(glue::glue("Fold-changes are only meaningful for factor/character, but {outcome} is a {class(d[[outcome]])}"))
+    if(length(unique(d_fc[[ outcome ]])) != 2)
+        stop(glue::glue("Fold-changes are only meaningful 2 groups, but {outcome} has {length(unique(d[[outcome]]))}"))
+
+    ## GET LEVELS IN ORDER OF FACTOR LEVELS
+    outcome_q <- rlang::sym(outcome)
+    if( "factor" %in% class(d_fc[[outcome]])){
+        levels <- levels(d_fc[[outcome]])
+    }else{
+        levels <- unique(d_fc[[outcome]])
+    }
+    levels_1  <- rlang::sym(levels[1])
+    levels_2  <- rlang::sym(levels[2])
+
+    ## FOLDCHANGE FUNCTION (CONSIDER PREVIOUS LOG)
+    d_fc <- d_fc %>%
+        gather(var, value, one_of(rownames(D))) %>%
+        group_by(var,!!outcome_q) %>%
+        summarise(value = combine(value)) %>%
+        ungroup() %>%
+        spread(!!outcome_q, value) %>%
+        mutate(fc = !!levels_1 - !!levels_2) %>%
+        select(var, fc)
+
+    ## ADD TO RESULTS
+    metadata(D)$results[[stat_id]]$output$table %<>%
+                  left_join(d_fc, by = "var")
+
+    ## add status information & plot
+    funargs <- mti_funargs()
+    metadata(D)$results %<>% 
+                  mti_generate_result(
+                      funargs = funargs,
+                      logtxt = sprintf("Calculate foldchanges for %s", statname),
+                      output = NULL
+                  )
+    ## RETURN
+    D
+}
+
