3c03fcb249853f0b61f2c04448faec1a3c9e3f4e
Parviz Gomari <parviz.gomari@gmail.com>
2019-01-17 22:30:36 +0100

    added mt_anno_pathways_from_file to load pathway annotations from flat files

--- mt_anno_pathways_from_file.R
+++ mt_anno_pathways_from_file.R
@@ -3,7 +3,7 @@
 # Add pathway information
 #
 # Adds custom pathways to the already existing SummarizedExperiment
-# data structure using the HMDB .xml file
+# data structure using a flatfile.
 #
 # last update: 2019-01-17
 # author: Parviz Gomari
@@ -16,49 +16,51 @@ library(tidyverse)
 library(data.table)
 
 # main
-mt_add_pathways_HMDB <- function(
+mt_anno_pathways_from_file <- function(
   D,                  # SummarizedExperiment input
   in_col,             # column to use for pathway fetching. The selected column must contain metabolite identifiers (e.g. HMBD, KEGG, ChEBI, etc)
   out_col,            # a new column name for D to output pathway information to
-  pwdb_name = "SMP",  # the name of the pathway database to use. Can use either SMP or KEGG for this argument
-  db_dir,             # name of the directory where the parsed HMDB files are stored
-  db_filename,        # filename of the parsed HMDB file to use. Must be in the format: hmdb_preprocessed_{version_number}.rds
+  flatfile,           # path where the pathway annotation flat file is stored
+  met_ID_col,         # flatfile colname: this column should contain metabolite IDs
+  pw_ID_col,          # flatfile colname: this column should contain pathway IDs
+  pw_name_col,        # flatfile colname: this column should contain pathway names
   export_raw_db       # OPTIONAL. Export the pathway database to a directory. Must be a string containing the path name with a .xlsx extension.
 ) {
   
   # check arguments
   stopifnot("SummarizedExperiment" %in% class(D))
   
-  if (missing(db_dir))
-    stop("db_dir must be given to fetch pathway database files from")
+  if (missing(flatfile))
+    stop("flatfile must be given to fetch pathway annotation file from")
   
-  if (!dir.exists(db_dir))
-    stop(glue::glue("{db_dir} does not exist. input a valid db_dir."))
-  
-  if (missing(db_filename)) {
-    # NOTE: taking the tail work only for HMDB versions below 9, since
-    # list.files sorts the names alphabetically. Current HMDB version
-    # is 4.0
-    db_filename <- list.files(db_dir) %>% tail(1)
-    if (length(db_filename) == 0) 
-      stop(glue::glue("no pathway files were found in {db_dir}"))
-  }
-  
-  if (!pwdb_name %in% c("SMP", "KEGG"))
-    stop(glue::glue("pwdb_name is invalid. please use either SMP or KEGG for pwdb_name."))
+  if (!file.exists(flatfile))
+    stop(glue::glue("{flatfile} does not exist. input a valid flatfile path."))
   
   if(!in_col %in% names(rowData(D)))
     stop(glue::glue("in_col is invalid. please enter an existing column name."))
   
-  # HMDB IDs here pertain to secondary accessions
-  mti_logstatus(glue::glue("reading the {pwdb_name} database from {db_filename}"))
+  # check flatfile colnames
   pwdb <- 
-    read_rds(file.path(db_dir, db_filename)) %>% 
-    dplyr::select(HMDB_id, ID = pwdb_name, pathway_name, accession)
+    fread(flatfile) 
+  
+  flatfile_cols <- c(met_ID_col, pw_ID_col, pw_name_col)
+  valid_names <- flatfile_cols %in% names(pwdb)
+  if (!all(valid_names)) {
+    invalid_names <- flatfile_cols[!valid_names]
+    stop(sprintf("Non-existent flatfile column names. Please replace: %s \n with one of: %s", 
+                 str_c(invalid_names, collapse = ", "),
+                 str_c(names(pwdb), collapse = ", ")))
+  }
+  
+  pwdb %<>% 
+    dplyr::select(met_ID = !!met_ID_col, 
+                  ID = !!pw_ID_col,
+                  pathway_name = !!pw_name_col)
   
   # create a dataframe that enables the mapping between pathway
   # names and IDs. Included also are num_total, num_measured,
   # num_pw_total, and num_pw_measured (see below for further details)
+  # in this dataframe map
   
   # num_total - overall number of metabolites in entire database (this will 
   # be a redundant, repeating number, identical in every row… but it’s the 
@@ -66,20 +68,20 @@ mt_add_pathways_HMDB <- function(
   # pwdb$accession is used here, since there is a many-to-one mapping 
   # between HMDB_id (secondary accessions) and accession
   num_total <- 
-    pwdb$accession %>% 
+    pwdb$met_ID %>% 
     unique() %>% 
     length()
   
   # num_measured - overall number of measured metabolites found in the DB
   num_measured = 
-    pwdb$HMDB_id %>% 
+    pwdb$met_ID %>% 
     # for this intersect, similar to the one in pwdb_summary (below),
-    # it is assumed that the HMDB IDs in the dataset D is non-redundant.
+    # it is assumed that the IDs in the dataset D is non-redundant.
     # else this number may not be accurate.
     intersect(rowData(D)[[in_col]]) %>% 
     length()
   
-  mti_logstatus(glue::glue("summarizing {pwdb_name} pathway information"))
+  mti_logstatus(glue::glue("summarizing pathway information"))
   pwdb_summary <- pwdb
   # using methods from data.table reduces runtime by almost 10x as compared
   # to dplyr
@@ -92,28 +94,28 @@ mt_add_pathways_HMDB <- function(
       # num_pw_total - the total number of metabolites in that pathway 
       # here, accession is used.
       # (overall DB background)
-      num_pw_total = uniqueN(accession), 
+      num_pw_total = uniqueN(met_ID), 
       
       # num_pw_measured - the number of measured metabolites in that pathway 
       # (for the M type of analysis on the actual measured background).
       num_pw_measured = 
-        sum(HMDB_id %in% rowData(D)[[in_col]], na.rm = TRUE)
+        sum(met_ID %in% rowData(D)[[in_col]], na.rm = TRUE)
     ),
     by = ID] %>% 
     # some IDs might have more than two names, however, these will be discarded
     # for now
     unique(by = c("ID")) %>% 
     subset(!is.na(ID), 
-           select = -c(HMDB_id, accession))
+           select = -met_ID) 
   
   
-  mti_logstatus(glue::glue("nesting {pwdb_name} pathway IDs"))
-  # nest all the pathway IDs given our lieblings input IDs (HMDB IDs for now)
+  mti_logstatus(glue::glue("nesting pathway IDs"))
+  # nest all the pathway IDs given our lieblings input IDs 
   pwdb_reduced <- 
     pwdb %>%
-    group_by(HMDB_id) %>% 
+    group_by(met_ID) %>% 
     filter(!is.na(ID)) %>% 
-    distinct(HMDB_id, ID) %>% 
+    distinct(met_ID, ID) %>% 
     nest(ID, .key = ID) %>% 
     mutate(ID = 
              ID %>%
@@ -121,11 +123,10 @@ mt_add_pathways_HMDB <- function(
              as.list())
   
   
-  
   # match the nested pathways to our lieblings IDs
   match_idx <- 
     match(rowData(D)[[in_col]],
-          pwdb_reduced$HMDB_id)
+          pwdb_reduced$met_ID)
   
   pw_col <- pwdb_reduced$ID[match_idx]
   
@@ -146,7 +147,7 @@ mt_add_pathways_HMDB <- function(
   metadata(D)$results %<>% 
     mti_generate_result(
       funargs = funargs,
-      logtxt = sprintf('added pathway annotations using the %s pathway database', pwdb_name)
+      logtxt = sprintf('added pathway annotations using the %s flat file', flatfile)
     )
   
   D
@@ -159,7 +160,10 @@ if (FALSE) {
   mt_logging(console=T) 
   D_alone <- 
     mt_files_load_metabolon(codes.makepath("packages/metabotools/sampledata.xlsx"), "OrigScale") %>% 
-    mt_add_pathways_HMDB(in_col = "HMDb_ID", out_col = "smp_db", 
-                         pwdb_name = "SMP", db_dir = codes.makepath("packages/metabotools_external/hmdb"))
+    mt_anno_pathways_from_file(
+      in_col = "HMDb_ID", out_col = "smp_db", 
+      flatfile = codes.makepath("packages/metabotools_external/hmdb/hmdb_preprocessed_4.0.csv"),
+      met_ID_col = "HMDB_id", pw_ID_col = "SMP", pw_name_col = "pathway_name"
+    ) 
 }
 
