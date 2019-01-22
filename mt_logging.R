80a9ba71d88d3aa064d04f870d0e1582a17f7c54
Unknown <krumsiek@gmx.net>
2019-01-22 17:23:40 -0500

    bugfix for new version of logger package
    logger interprets text as format in new version

--- mt_logging.R
+++ mt_logging.R
@@ -21,10 +21,12 @@ require(logging)
 
 # helper function that sends an info message to the "mt" logger
 # function also returns the string again, so it can be directly used to store log messages as well
-mti_logmsg <- function(msg) { loginfo(msg, logger="mt"); msg }
-mti_logstatus <- function(msg) { loginfo(msg, logger="mts"); msg }
-mti_logwarning <- function(msg) { loginfo(sprintf("WARNING: %s",msg), logger="mtw"); msg }
+mti_logmsg <- function(msg) { loginfo(mti_escape_percent(msg), logger="mt"); msg }
+mti_logstatus <- function(msg) { loginfo(mti_escape_percent(msg), logger="mts"); msg }
+mti_logwarning <- function(msg) { loginfo(mti_escape_percent(sprintf("WARNING: %s",msg)), logger="mtw"); msg }
 
+# produce sprintf-safe copy of string (create for new loginfo() behaviour, that interprets strings as formats)
+mti_escape_percent <- function(txt) gsub('%','%%',txt)
 
 # main function definition
 mt_logging <- function(
