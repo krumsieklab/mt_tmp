311ae50ce11b546616f3dd4f0fe86f534d1a82fc
Unknown <krumsiek@gmx.net>
2019-02-10 17:42:29 -0500

    MT: compare2stats can now compare results from two different SE

--- mt_plots_compare2stats.R
+++ mt_plots_compare2stats.R
@@ -1,25 +1,28 @@
 library(ggrepel)
 
 mt_plots_compare2stats <- function(
-  D,
+  D1,
   stat1,
   filter1,
   stat2,
   filter2,
-  filterop="AND"
+  filterop="AND",
+  D2 = D1,
+  return.plot.only=F  # return only the plot object. note: setting this to true makes the function non-MT pipeline compatible.
 ) {
   
   ## check input
-  stopifnot("SummarizedExperiment" %in% class(D))
+  stopifnot("SummarizedExperiment" %in% class(D1))
+  stopifnot("SummarizedExperiment" %in% class(D2))
   filter1q <- enquo(filter1)
   filter2q <- enquo(filter2)
   
   if (!(filterop %in% c("AND","OR"))) stop("filterop must be 'AND' or 'OR'")
   
   ## obtain the two stats structures
-  s1 <- mti_get_stat_by_name(D, stat1, fullstruct=T)
+  s1 <- mti_get_stat_by_name(D1, stat1, fullstruct=T)
   s1t <- s1$table
-  s2 <- mti_get_stat_by_name(D, stat2, fullstruct=T)
+  s2 <- mti_get_stat_by_name(D2, stat2, fullstruct=T)
   s2t <- s2$table
   
   ## add directed p-value, filter, and merge
@@ -28,7 +31,10 @@ mt_plots_compare2stats <- function(
   s2t$dp2 <- -log10(s2t$p.value) * sign(s2t$statistic)
   s2t$filtered2 <- s2t$var %in% (s2t %>% filter(!!filter2q))$var
   st <- merge(s1t, s2t, by='var')
-  st <- merge(st, rowData(D), by.x="var", by.y='row.names') # add names
+  
+  
+  st <- merge(st, rowData(D1), by.x="var", by.y='row.names', all.x=T) # add names
+  
   
   # combine filters
   if (filterop=="AND")
@@ -39,7 +45,7 @@ mt_plots_compare2stats <- function(
     st$filtered = as.numeric(st$filtered1) + as.numeric(st$filtered2)
   else
     stop("bug")
-    
+  
   ## create axis labels
   if ("groups" %in% names(s1) && length(s1$groups)==2) {
     xlabel = sprintf("%s high <--   dir. log10(p-value)   --> %s high", s1$groups[1], s1$groups[2])
@@ -61,17 +67,22 @@ mt_plots_compare2stats <- function(
     labs(color='filtered') +
     geom_text_repel(data=filter(st, filtered>0), aes(label=name), size=3, colour = "black") + 
     xlab(xlabel) + ylab(ylabel)
-
   
-  ## add status information & plot
-  funargs <- mti_funargs()
-  metadata(D)$results %<>% 
-    mti_generate_result(
-      funargs = funargs,
-      logtxt = sprintf("comparison plot between '%s' and '%s'", stat1, stat2),
-      output = list(p)
-    )
-  ## return
-  D
+  
+  if (!return.plot.only) {
+    ## add status information & plot
+    funargs <- mti_funargs()
+    metadata(D1)$results %<>% 
+      mti_generate_result(
+        funargs = funargs,
+        logtxt = sprintf("comparison plot between '%s' and '%s'", stat1, stat2),
+        output = list(p),
+        output2 = st
+      )
+    ## return
+    D1
+  } else {
+    p
+  }
   
 }
