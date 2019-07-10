1dc5bb4b4f176c50b0b4a67bdab20ef04a3d301f
Jan K <krumsiek@gmx.net>
2019-07-10 14:49:36 -0400

    Major updates to the PCA function.

--- mt_plots_PCA.R
+++ mt_plots_PCA.R
@@ -1,4 +1,5 @@
 require(ggplot2)
+require(ggrepel)
 
 #' 2D PCA of samples.
 #' 
@@ -7,6 +8,13 @@ require(ggplot2)
 #' @param D \code{SummarizedExperiment} input
 #' @param title Title of the plot, default: "PCA"
 #' @param scaledata  scale data before plotting? (mean 0, std 1), default: FALSE
+#' @param PCa first PC to plot, default is 1 (PC1)
+#' @param PCb second PC to plot, default is 2 (PC2)
+#' @param showscores show 'scores' or 'loadings' 
+#' @param labelby field to label by. default: none
+#' @param textrepel try to avoid all text overlaps when labeling? default:T 
+#' @param ellipse confidence interval for ellipse. default: none (no ellipse)
+#' @param expvarplot add explained variance plot? default: F
 #' @param ggadd further elements/functions to add (+) to the ggplot object
 #' @param ... # additional expression directly passed to aes() of ggplot, can refer to colData
 #'
@@ -16,12 +24,19 @@ require(ggplot2)
 #' ## PCA on scaledata, color and shape by "Group" variable in colData
 #' ... $>$ mt_plots_PCA(scaledata=T, color=Group, shape=Group, title="PCA - scaled data") %>% ...
 #' 
-#' @author JK
+#' @author JK, BGP
 #' 
 mt_plots_PCA <- function(
   D,           
   title="PCA",    
   scaledata=F,
+  PCa=1,
+  PCb=2,
+  show='scores',
+  labelby='',
+  textrepel=T,
+  ellipse=NA,
+  expvarplot=F,
   ggadd=NULL, 
   ...           
 ) {
@@ -32,6 +47,8 @@ mt_plots_PCA <- function(
   # extract data and verify that there are no NA values
   X = t(assay(D))
   if (any(is.na(X))) stop("Data matrix for PCA cannot contain NAs")
+  # check that show is either "scores" or "loadings"
+  if (!(show %in% c("scores","loadings"))) stop("show must be either 'scores' or 'loadings'")
   
   # scale?
   if (scaledata) X <- scale(X)
@@ -40,26 +57,51 @@ mt_plots_PCA <- function(
   pca <- prcomp(x=as.matrix(X), center=F, scale=F) 
   expvar = (pca$sdev)^2 / sum(pca$sdev^2) 
   # assemble data frame, two PCS and sample info
-  df = data.frame(PC1 = pca$x[,1], PC2 = pca$x[,2], colData(D))
+  if (show=='scores') df = data.frame(x = pca$x[,PCa], y = pca$x[,PCb], colData(D)) # scores and colData
+  else  df = data.frame(x = pca$rotation[,PCa], y = pca$rotation[,PCb], rowData(D)) # loadings and rowData
+  colnames(df)[1:2] <- c(sprintf("PC%d",PCa),sprintf("PC%d",PCb))
   
   # prep
-  pc1name = sprintf('PC1 (%.1f%%)', expvar[1]*100)
-  pc2name = sprintf('PC2 (%.1f%%)', expvar[2]*100)
+  pc1name = sprintf('PC%d (%.1f%%)', PCa, expvar[1]*100)
+  pc2name = sprintf('PC%d (%.1f%%)', PCb, expvar[2]*100)
   
   # plot
   p <- ggplot(data=df) +
-    geom_point(aes(x=PC1,y=PC2,...)) + 
+    geom_point(aes_string(x=sprintf("PC%d",PCa),y=sprintf("PC%d",PCb),...)) + 
     xlab(pc1name) + ylab(pc2name) + ggtitle(title)
+  # add text?
+  if (nchar(labelby)>0) {
+    if (textrepel) p <- p + geom_text_repel(aes_string(x=sprintf("PC%d",PCa),y=sprintf("PC%d",PCb),label=labelby,...))
+    else p <- p + geom_text(aes_string(x=sprintf("PC%d",PCa),y=sprintf("PC%d",PCb),label=labelby,...))
+  }
+  # add ellipse?
+  if (!is.na(ellipse)) p <- p + stat_ellipse(aes_string(x=sprintf("PC%d",PCa),y=sprintf("PC%d",PCb)), level=ellipse)
   # add custom elements?
   if (!is.null(ggadd)) p <- p+ggadd
+  
+  # add explained variance plot?
+  plotlist <- list(p)
+  if (expvarplot) {
+    nlimit <- 10
+    expdf <- data.frame(n=1:length(expvar), expvar=expvar*100)
+    # cut to first nlimit at most
+    expdf <- expdf[1:min(nlimit,nrow(expdf)),]
+    newp <- ggplot(data=expdf, aes(x=n, y=expvar)) +
+      geom_bar(stat="identity", fill="steelblue")+
+      ylab("") + xlab("PC") + ggtitle(sprintf("Explained variance (first %d PCs)", nrow(expdf))) +
+      geom_text(aes(label=sprintf('%.1f%%',expdf$expvar)), vjust=-0.3, size=3.5) + 
+      scale_x_discrete(limits=1:nrow(expdf))
+    # add to list of plots for results
+    plotlist[[2]] <- newp
+  }
   
   # add status information & plot
   funargs <- mti_funargs()
   metadata(D)$results %<>% 
     mti_generate_result(
       funargs = funargs,
-      logtxt = sprintf("PCA, aes: %s", mti_dots_to_str(...)),
-      output = list(p)
+      logtxt = sprintf("PCA, %s, labelby: %s, aes: %s", show, labelby,  mti_dots_to_str(...)),
+      output = plotlist
     )
   
   # return
