bc7789134dd982a6b3c707709347a6ae24b256cb
Parviz Gomari <parviz.gomari@gmail.com>
2019-08-06 22:24:36 +0200

    added pathway enrichment for statistics calculated by mt_stats_univ_lm

--- mt_stats_pathway_enrichment.R
+++ mt_stats_pathway_enrichment.R
@@ -1,17 +1,15 @@
-#' Generate aggregated pathway values.
+#' Pathway enrichment script using statistical analysis results from metabotools pipeline.
 #' 
-#' Takes a pathway annotation column of the metabolites (rowData) and builds one representative sample per pathway for each sample.
-#'
+#' This script runs a pathway enrichment analysis using Fisher's exact test from statistics
+#' calculated by mt_stats_univ_lm().
+#' 
 #' Implemented approaches:
-#' 1. GAGE: https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-10-161
-#' 2. Fisher's exact test. Will NOT scale() data before. Data matrix can have NAs.
+#' 1. Fisher's exact test. 
 #' 
 #' @param D  \code{SummarizedExperiment} input
 #' @param pw_col column containing pathways IDs
-#' @param grouping_var column name of column containing grouping variables
-#' @param control_grp_name name(s) of control group(s) found in grouping_var column
-#' @param case_grp_name name(s) of case/phenotype group(s) found in grouping_var column
-#' @param method one of: "gage", "fishers"
+#' @param stat_name name of statistical analysis to use with mti_get_stat_by_name()
+#' @param cutoff cutoff to use for assigning whether a metabolite/gene is significant. Used in Fisher's exact test
 #'
 #' @return $pathways$enrichment_results: a dataframe containing the pathway enrichment results
 #' 
@@ -26,20 +24,16 @@
 #' 
 
 library(tidyverse)
-library(gage)
 
 
 mt_stats_pathway_enrichment <- function(
   D,          # SummarizedExperiment input
-  pw_col,       # string, column name for pathway annotations
-  grouping_var,
-  control_grp_name,
-  case_grp_name,
-  method = "gage"
+  pw_col,      # string, column name for pathway annotations
+  stat_name,
+  cutoff = 0.05
 ) {
   
   stopifnot("SummarizedExperiment" %in% class(D))
-  if (!method %in% c("fishers","gage")) stop("'method' must be either 'gage' or 'fishers'")
   
   meta_D <- metadata(D)
   
@@ -47,6 +41,8 @@ mt_stats_pathway_enrichment <- function(
   
   # Check if given pathway column actually exists
   if (!pw_col %in% names(meta_D$pathways)) stop(sprintf("'%s' not found in metabolite annotations.", pw))
+  
+  # have a check for wheter stat_name exists in D?
   
   pw_id_map <- 
     meta_D$pathways[[pw_col]] %>% 
@@ -62,145 +58,51 @@ mt_stats_pathway_enrichment <- function(
     filter(!!rlang::sym(pw_col) != "NA") %>% 
     distinct()
   
-  pw_names <- 
-    geneset[[pw_col]] %>% 
-    unique()
   
-  geneset_list <- 
-    sapply(
-      pw_names, 
-      simplify = FALSE,
-      USE.NAMES = TRUE,
-      function(pw_name) {
-        geneset %>% 
-          filter(!!rlang::sym(pw_col) == pw_name) %>% 
-          .$COMP_IDstr
-      }
-    )
+  # Algorithm summary:
+  # - calculate metabolite p-values per group
+  # - adjust p-values using FDR
+  # - assign significance to adjusted p-value at 0.05 level
+  # - perform Fisher's exact test
+  # - adjust Fisher's exact test p-value
+  # - calculate mean fold change based on mean log value of cases and ctrls
   
-  # create indices for control and case groups
-  # NOTE: it is assumed here that the order of sample groups in colData
-  # follows the order of samples in the assay dataframe
-  sample_indices <- 
-    colData(D) %>% 
-    as.data.frame() %>%
-    dplyr::transmute(!!rlang::sym(grouping_var),
-                     sample_idx = row_number())
-  
-  ctrl_samples <- 
-    sample_indices %>% 
-    filter(!!rlang::sym(grouping_var) %in% control_grp_name) %>% 
-    .$sample_idx
-  
-  case_samples <- 
-    sample_indices %>% 
-    filter(!!rlang::sym(grouping_var) %in% case_grp_name) %>% 
-    .$sample_idx
-  
-  
-  if (method == "gage") {
+  enrichment_results <- 
+    mti_get_stat_by_name(D, stat_name) %>% 
     
-    # perform pathway enrichment using the GAGE package
-    enrichment_results <- 
-      assay(D) %>% 
-      as.data.frame() %>% 
-      gage(gsets = geneset_list,
-           ref = ctrl_samples,
-           samp = case_samples,
-           same.dir = FALSE, # suggsted for KEGG pathways
-           compare = "unpaired", # unpaired samples
-           set.size = c(5, 500)) %>% 
-      .$greater %>% 
-      as.data.frame() %>%
-      rownames_to_column("pathway_ID") %>% 
-      dplyr::select(pathway_ID,
-                    p_value = p.val, 
-                    p_value_adjusted = q.val,
-                    mean_foldchange = stat.mean) %>% 
-      filter(!is.na(p_value)) %>% 
-      
-      left_join(pw_id_map, by = c("pathway_ID" = "ID")) %>% 
-      dplyr::select(pathway_name, everything()) 
+    # assign significance
+    dplyr::mutate(significant = if_else(p.adj < cutoff, TRUE, FALSE),
+                  n_total = n(),
+                  n_total_sig = sum(significant)) %>% 
+    inner_join(geneset, by = c("var" = "COMP_IDstr")) %>% 
+    group_by(!!rlang::sym(pw_col)) %>% 
     
+    # calculate summary numbers for Fisher's test
+    dplyr::summarise(n_total = unique(n_total),
+                     n_total_sig = unique(n_total_sig),
+                     n_pw = n(),
+                     n_pw_sig = sum(significant),
+                     mean_fc = mean(fc)) %>% 
+    filter(n_pw >= 5) %>% 
     
-  } else if (method == "fishers") {
-    
-    # Algorithm summary:
-    # - calculate metabolite p-values per group
-    # - adjust p-values using FDR
-    # - assign significance to adjusted p-value at 0.05 level
-    # - perform Fisher's exact test
-    # - adjust Fisher's exact test p-value
-    # - calculate mean fold change based on mean log value of cases and ctrls
-    
-    met_stats <- 
-      assay(D) %>% 
-      t() %>% 
-      as.data.frame() %>% 
-      
-      # assign sample index
-      # NOTE: the assumption here is that each row is ordered 
-      # as the rows in colData
-      dplyr::mutate(sample_idx = row_number()) %>% 
-      
-      # select samples
-      filter(sample_idx %in% c(ctrl_samples, case_samples)) %>% 
-      dplyr::mutate(grp = if_else(sample_idx %in% ctrl_samples, "ctrl", "case")) %>% 
-      dplyr::select(-sample_idx) %>% 
-      
-      # Calculate p-values and the group means
-      summarise_if(is.numeric, list(p_value = ~ t.test(. ~ .data$grp)$p.value,
-                                    mean_ctrl = ~ t.test(. ~ .data$grp)$estimate %>% as.numeric() %>% .[1],
-                                    mean_case = ~ t.test(. ~ .data$grp)$estimate %>% as.numeric() %>% .[2])) %>% 
-      gather(met_id, vals) %>% 
-      separate(met_id, c("met_id", "val_type"), "_", extra = "merge") %>% 
-      spread(val_type, vals) %>% 
-      dplyr::mutate(p_value_adjusted = p.adjust(p_value, method = "fdr"))
-    
-    enrichment_results <- 
-      met_stats %>% 
-      
-      # assign significance
-      dplyr::mutate(significant = if_else(p_value_adjusted < 0.05, TRUE, FALSE),
-                    n_total = n(),
-                    n_total_sig = sum(significant)) %>% 
-      inner_join(geneset, by = c("met_id" = "COMP_IDstr")) %>% 
-      group_by(!!rlang::sym(pw_col)) %>% 
-      
-      # calculate summary numbers for Fisher's test
-      dplyr::summarise(n_total = unique(n_total),
-                       n_total_sig = unique(n_total_sig),
-                       n_pw = n(),
-                       n_pw_sig = sum(significant),
-                       mean_case = mean(mean_case),
-                       mean_ctrl = mean(mean_ctrl)) %>% 
-      filter(n_pw >= 5) %>% 
-      
-      # calculate contingency table entries
-      dplyr::mutate(s_p = n_pw_sig,
-                    ns_p = n_pw - n_pw_sig,
-                    s_np = n_total_sig - n_pw_sig,
-                    ns_np = n_total - (s_p + ns_p + s_np)) %>% 
-      rowwise() %>% 
-      dplyr::mutate(p_value = 
-                      fisher.test(matrix(c(s_p, s_np, ns_p, ns_np), nrow = 2)) %>% 
-                      .$p.value) %>% 
-      ungroup() %>% 
-      dplyr::rename(ID = !!rlang::sym(pw_col)) %>% 
-      left_join(pw_id_map, by = "ID") %>% 
-      dplyr::transmute(pathway_name, 
-                       pathway_ID = ID,
-                       p_value, 
-                       p_value_adjusted = p.adjust(p_value, method = "fdr"), 
-                       mean_foldchange = mean_case - mean_ctrl) %>% 
-      arrange(p_value)
-    
-  } else {
-    
-    stop("Bug.")
-    
-  }
-  
+    # calculate contingency table entries
+    dplyr::mutate(s_p = n_pw_sig,
+                  ns_p = n_pw - n_pw_sig,
+                  s_np = n_total_sig - n_pw_sig,
+                  ns_np = n_total - (s_p + ns_p + s_np)) %>% 
+    rowwise() %>% 
+    dplyr::mutate(p_value = 
+                    fisher.test(matrix(c(s_p, s_np, ns_p, ns_np), nrow = 2)) %>% 
+                    .$p.value) %>% 
+    ungroup() %>% 
+    dplyr::rename(ID = !!rlang::sym(pw_col)) %>% 
+    left_join(pw_id_map, by = "ID") %>% 
+    dplyr::transmute(pathway_name, 
+                     pathway_ID = ID,
+                     p_value, 
+                     p_value_adjusted = p.adjust(p_value, method = "fdr"), 
+                     mean_foldchange = mean_fc) %>% 
+    arrange(p_value)
   
   metadata(D)$pathways$enrichment_results <- 
     as_tibble(enrichment_results)
@@ -210,8 +112,8 @@ mt_stats_pathway_enrichment <- function(
   metadata(D)$results %<>% 
     mti_generate_result(
       funargs = funargs,
-      logtxt = sprintf('performed pathway enrichment on %s pathways using "%s"', 
-                       nrow(enrichment_results), method)
+      logtxt = sprintf("performed pathway enrichment on %s pathways using Fihser's exact test", 
+                       nrow(enrichment_results))
     )
   
   D
@@ -237,17 +139,27 @@ if (FALSE) {
     # logging
     mt_pre_trans_log() %>%
     # KNN imputation
-    mt_pre_impute_knn()
+    mt_pre_impute_knn() %>% 
+    # linear model, differential test on Group
+    mt_stats_univ_lm(
+      formula      = ~ Group, 
+      samplefilter = (Group %in% c("treatment1","treatment2")),
+      name         = "comp",
+      mc.cores     = 1
+    ) %>%
+    # add fold changes to result tables
+    mt_post_addFC(statname = "comp") %>%
+    # add multiple testing correction
+    mt_post_multTest(statname = "comp", method = "BH")
   
   
   D_pw <- 
     D_pre %>% 
-    mt_stats_pathway_enrichment("kegg_db", 
-                                grouping_var = "Group", 
-                                control_grp_name = "Vehicle", 
-                                case_grp_name = c("treatment2", "treatment1"),
-                                method = "gage")
+    mt_stats_pathway_enrichment("kegg_db",
+                                stat_name = "comp",
+                                cutoff = 0.4)
   
   metadata(D_pw)$pathways$enrichment_results
+  
 }
 
