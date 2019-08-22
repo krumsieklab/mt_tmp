8334b04c4b9d484d9153b12f7a0690e764ea41d5
Jan K <krumsiek@gmx.net>
2019-08-22 15:57:04 -0400

    major performance improvement for boxplot function

--- mt_plots_boxplot.R
+++ mt_plots_boxplot.R
@@ -38,164 +38,174 @@ require(glue)
 #'                  
 #'  @author Jonas Zierer
 mt_plots_boxplot <- function(D,
-                            x = "x",
-                            statname,
-                            correct_confounder,
-                            metab_filter = p.value < 0.05,
-                            metab_sort   = p.value,
-                            annotation   = "{sprintf('P-value: %.1e', p.value)}",
-                            text.size = 3.88,
-                            jitter       = T,
-                            rows,
-                            cols,
-                            restrict_to_groups=T,
-                            ggadd        = NULL,
-                            ...){
+                             x = "x",
+                             statname,
+                             correct_confounder,
+                             metab_filter = p.value < 0.05,
+                             metab_sort   = p.value,
+                             annotation   = "{sprintf('P-value: %.1e', p.value)}",
+                             text.size = 3.88,
+                             jitter       = T,
+                             rows,
+                             cols,
+                             restrict_to_groups=T,
+                             ggadd        = NULL,
+                             ...){
+  
+  stopifnot("SummarizedExperiment" %in% class(D))
+  x <- enquo(x)
+  
+  
+  ## CONFOUNDER
+  if(!missing(correct_confounder)){
+    mti_logstatus(glue::glue("correcting for {correct_confounder}"))
+    D <- mti_correctConfounder(D, correct_confounder)
+  }
+  
+  ## rowData
+  rd <- rowData(D) %>%
+    as.data.frame() %>%
+    mutate(var = rownames(D))
+  
+  ## stat
+  if(!missing(statname)){
+    stat <- mti_get_stat_by_name(D, statname) %>%
+      inner_join(rd, by = "var")
+  }else{
+    stat <- rd
+    restrict_to_groups <- F # not dependend on a stat
+  }
+  
+  ## FILTER METABOLITES
+  if(!missing(metab_filter)){
+    metab_filter_q <- enquo(metab_filter)
+    stat <- stat %>%
+      filter(!!metab_filter_q)
+    mti_logstatus(glue::glue("filter metabolites: {metab_filter_q} [{nrow(stat)} remaining]"))
+  }
+  
+  ## SORT METABOLITES
+  if(!missing(metab_sort)){
+    metab_sort_q <- enquo(metab_sort)
+    stat <- stat %>%
+      arrange(!!metab_sort_q) %>%
+      ## sort according to stat
+      mutate(name = factor(name, levels = unique(name)))
+    mti_logstatus(glue::glue("sorted metabolites: {metab_sort_q}"))
+  }
+  
+  
+  
+  ## CREATE PLOT
+  dummy <- D %>%
+    mti_format_se_samplewise() %>%
+    gather(var, value, one_of(rownames(D)))
+  ## filter to groups?
+  if (restrict_to_groups) {
+    filterto <- mti_get_stat_by_name(D, statname, fullstruct=T)$groups
+    dummy <- dummy[dummy[[stat$term[1]]] %in% filterto,]
+  }
+  
+  
 
-    stopifnot("SummarizedExperiment" %in% class(D))
-    x <- enquo(x)
-    
-
-    ## CONFOUNDER
-    if(!missing(correct_confounder)){
-        mti_logstatus(glue::glue("correcting for {correct_confounder}"))
-        D <- mti_correctConfounder(D, correct_confounder)
-    }
-    
-    ## rowData
-    rd <- rowData(D) %>%
-        as.data.frame() %>%
-        mutate(var = rownames(D))
-    
-    ## stat
-    if(!missing(statname)){
-        stat <- mti_get_stat_by_name(D, statname) %>%
-            inner_join(rd, by = "var")
-    }else{
-        stat <- rd
-        restrict_to_groups <- F # not dependend on a stat
-    }
-
-    ## FILTER METABOLITES
-    if(!missing(metab_filter)){
-        metab_filter_q <- enquo(metab_filter)
-        stat <- stat %>%
-            filter(!!metab_filter_q)
-          mti_logstatus(glue::glue("filter metabolites: {metab_filter_q} [{nrow(stat)} remaining]"))
-    }
-
-    ## SORT METABOLITES
-    if(!missing(metab_sort)){
-        metab_sort_q <- enquo(metab_sort)
-        stat <- stat %>%
-            arrange(!!metab_sort_q) %>%
-            ## sort according to stat
-            mutate(name = factor(name, levels = unique(name)))
-        mti_logstatus(glue::glue("sorted metabolites: {metab_sort_q}"))
-    }
-    
-   
-
-    ## CREATE PLOT
-    dummy <- D %>%
-        mti_format_se_samplewise() %>%
-        gather(var, value, one_of(rownames(D)))
-    ## filter to groups?
-    if (restrict_to_groups) {
-      filterto <- mti_get_stat_by_name(D, statname, fullstruct=T)$groups
-      dummy <- dummy[dummy[[stat$term[1]]] %in% filterto,]
-    }
-    # else {
-    #   filterto <- unique(dummy[[stat$term[1]]])
-    # }
- 
-    #
-    plottitle <- ifelse(missing(statname),"",statname)
-    p <- dummy %>%
-        ## add metabolite names
-        inner_join(stat, by = "var") %>% 
-        ## do plot
-        ggplot() +
-        geom_boxplot(aes(x = !!x, y = value, ...), outlier.shape = ifelse(jitter, NA, 19)) +
-        labs(x = NULL, y = NULL) +
-        ggtitle(plottitle)
-    
-    ## add ylabel if this is logged data
-    r <- D %>% mti_res_get_path(c("pre","trans","log"))
-    if (length(r)>0) {
-      p <- p + ylab(r[[1]]$logtxt) # log text contains e.g. "log2"
-    }
-
-    ## ADD JITTER
-    if(jitter){
-        p <- p +
-            ggbeeswarm::geom_beeswarm(aes(x = !!x, y = value, ...))
-    }
-    
-    ## ADD ANNOTATION
-    if(!missing(annotation)){
-        data_annotate <- stat %>%
-            mutate(annotate = glue::glue(annotation)) %>%
-            distinct(name, annotate) 
-        p <- p + geom_text(data = data_annotate,
-                           aes(label = annotate),
-                           x = -Inf, y = Inf, hjust = -0.05, vjust = 1.05, size=text.size )
-    }
-    
-    if (!is.null(ggadd)) p <- p+ggadd
-
-    ## SPLIT TO MULTIPLE PAGES
-    # if there is no plot, create a single empty page
-    if (length(unique(stat$name))==0) {
-      p <- list(ggplot() + geom_text(aes(x=0, y=0, label='no plots'), size=10))
-    } else {
-      if(!missing(cols) && !missing(rows)){
-        p_plots   <- length(unique(stat$name))
-        p_perpage <- cols*rows
-        pages     <- ceiling(p_plots / p_perpage)
-        mti_logstatus(glue::glue("split {p_plots} plots to {pages} pages with {rows} rows and {cols} cols"))
-        p <- map(1:pages, ~ p + ggforce::facet_wrap_paginate(~name, scales = "free_y", nrow = rows, ncol  = cols, page = .x))
-        ## ADD EMPTY PLOTS TO FILL PAGE
-        fill_page <- (pages*p_perpage) - p_plots
-        if(fill_page > 0){
-          mti_logstatus(glue::glue("add {fill_page} blanks to fill page"))
-          spaces <- map(1:fill_page, ~rep(" ", .x)) %>%
-            map_chr(str_c, collapse = "")
-          p[[ pages ]] <- p[[ pages ]] +
-            geom_blank(data = data.frame(name = spaces))
-        }
-      }else{
-        p <- list(p + facet_wrap(.~name, scales = "free_y"))
+  # filter down only to the variables needed for plotting
+  # need to parse x and ... list
+  vars <- x %>% as.character() %>% gsub("~","",.)
+  q <- quos(...)
+  if (length(q) > 0) {
+    vars <- c(vars, q %>% lapply(function(x){x %>% as.character() %>% gsub("~","",.)}) %>% unlist() %>% as.vector())
+  }
+  vars <- unique(vars)
+  
+  #
+  plottitle <- ifelse(missing(statname),"",statname)
+  p <- dummy %>%
+    dplyr::select(one_of(c("var","value", vars))) %>%
+    ## add metabolite names, but only restricted subset from statistics table
+    inner_join(stat[,c('var','statistic','p.value','p.adj','name')], by = "var") %>% 
+    dplyr::select(-var) %>%
+    ## do plot
+    ggplot() +
+    geom_boxplot(aes(x = !!x, y = value, ...), outlier.shape = ifelse(jitter, NA, 19)) +
+    labs(x = NULL, y = NULL) +
+    ggtitle(plottitle)
+  
+  ## add ylabel if this is logged data
+  r <- D %>% mti_res_get_path(c("pre","trans","log"))
+  if (length(r)>0) {
+    p <- p + ylab(r[[1]]$logtxt) # log text contains e.g. "log2"
+  }
+  
+  ## ADD JITTER
+  if(jitter){
+    p <- p +
+      ggbeeswarm::geom_beeswarm(aes(x = !!x, y = value, ...))
+  }
+  
+  ## ADD ANNOTATION
+  if(!missing(annotation)){
+    data_annotate <- stat %>%
+      mutate(annotate = glue::glue(annotation)) %>%
+      distinct(name, annotate) 
+    p <- p + geom_text(data = data_annotate,
+                       aes(label = annotate),
+                       x = -Inf, y = Inf, hjust = -0.05, vjust = 1.05, size=text.size )
+  }
+  
+  if (!is.null(ggadd)) p <- p+ggadd
+  
+  ## SPLIT TO MULTIPLE PAGES
+  # if there is no plot, create a single empty page
+  if (length(unique(stat$name))==0) {
+    p <- list(ggplot() + geom_text(aes(x=0, y=0, label='no plots'), size=10))
+  } else {
+    if(!missing(cols) && !missing(rows)){
+      p_plots   <- length(unique(stat$name))
+      p_perpage <- cols*rows
+      pages     <- ceiling(p_plots / p_perpage)
+      mti_logstatus(glue::glue("split {p_plots} plots to {pages} pages with {rows} rows and {cols} cols"))
+      p <- map(1:pages, ~ p + ggforce::facet_wrap_paginate(~name, scales = "free_y", nrow = rows, ncol  = cols, page = .x))
+      ## ADD EMPTY PLOTS TO FILL PAGE
+      fill_page <- (pages*p_perpage) - p_plots
+      if(fill_page > 0){
+        mti_logstatus(glue::glue("add {fill_page} blanks to fill page"))
+        spaces <- map(1:fill_page, ~rep(" ", .x)) %>%
+          map_chr(str_c, collapse = "")
+        p[[ pages ]] <- p[[ pages ]] +
+          geom_blank(data = data.frame(name = spaces))
       }
+    }else{
+      p <- list(p + facet_wrap(.~name, scales = "free_y"))
     }
-
-    ## add status information & plot
-    funargs <- mti_funargs()
-    metadata(D)$results %<>% 
-                  mti_generate_result(
-                      funargs = funargs,
-                      logtxt = sprintf("Metabolite boxplot, aes: %s", mti_dots_to_str(...)),
-                      output = p
-                  )
-    ## return
-    D
+  }
+  
+  ## add status information & plot
+  funargs <- mti_funargs()
+  metadata(D)$results %<>% 
+    mti_generate_result(
+      funargs = funargs,
+      logtxt = sprintf("Metabolite boxplot, aes: %s", mti_dots_to_str(...)),
+      output = p
+    )
+  ## return
+  D
 }
 
 
 mti_correctConfounder <- function(D, formula){
-    d <- D %>% mti_format_se_samplewise()
-    d_cor <- rownames(D) %>%
-        map_dfc(function(m){
-            f   <- update.formula(formula, str_c(m, "~."))
-            mod <- lm(f, data = d, na.action = na.exclude)
-            res <- resid(mod)
-            res
-        }) %>%
-        setNames(rownames(D)) %>%
-        as.matrix() %>% t()
-    colnames(d_cor) <- colnames(D)
-    assay(D)        <- d_cor
-    D
+  d <- D %>% mti_format_se_samplewise()
+  d_cor <- rownames(D) %>%
+    map_dfc(function(m){
+      f   <- update.formula(formula, str_c(m, "~."))
+      mod <- lm(f, data = d, na.action = na.exclude)
+      res <- resid(mod)
+      res
+    }) %>%
+    setNames(rownames(D)) %>%
+    as.matrix() %>% t()
+  colnames(d_cor) <- colnames(D)
+  assay(D)        <- d_cor
+  D
 }
 
 
@@ -203,4 +213,4 @@ mti_correctConfounder <- function(D, formula){
 
 
 
-            
+
