d171308b11cf8993f2bdb181ba1fc853a8a4b771
Karsten Suhre <kas2049@qatar-med.cornell.edu>
2020-03-07 10:38:49 +0300

    itesting

--- mt_karsten_test_ignore.R
+++ mt_karsten_test_ignore.R
@@ -1,3 +1,4 @@
 # this function does not do anything
 # it is just a test to see whether I can use GIT as expected
 # more serious stuff to follow soon ... all the best, Karsten
+cat("some test to check git is working properly\n")
