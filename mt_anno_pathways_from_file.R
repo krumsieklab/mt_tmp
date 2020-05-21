e91d0673a9575eeb4b81fd5e889cbc536f67d1cf
KelseyChetnik <kelsey.chetnik@gmail.com>
2020-05-21 15:40:31 -0400

    Fix forced merge problems
    Fixed forced merge problems for 3 files

--- mt_anno_pathways_from_file.R
+++ mt_anno_pathways_from_file.R
@@ -3,7 +3,7 @@ library(data.table)
 library(readxl)
 
 #' Add pathway information.
-#'
+#' 
 #' Adds custom pathways to the already existing SummarizedExperiment
 #' data structure using a flatfile.
 #'
@@ -23,17 +23,16 @@ library(readxl)
 #' @examples
 #' # annotate metabolites using the SMP column of the pathway database flatfile
 #' ... %>%
-#'       mt_anno_pathways_from_file(in_col = "HMDb_ID",
-#'                                  out_col = "janpw",
+#'       mt_anno_pathways_from_file(in_col = "HMDb_ID", 
+#'                                  out_col = "janpw", 
 #'                                  flatfile = codes.makepath("packages/metabotools_external/hmdb/hmdb_preprocessed_4.0.csv"),
-#'                                  met_ID_col = "HMDB_id",
-#'                                  pw_ID_col = "SMP",
+#'                                  met_ID_col = "HMDB_id", 
+#'                                  pw_ID_col = "SMP", 
 #'                                  pw_name_col = "pathway_name") %>%
 #' ...
-#'
+#' 
 #' @author Parviz Gomari
-#'
-#' @export
+#' 
 
 # main
 mt_anno_pathways_from_file <- function(
@@ -47,161 +46,130 @@ mt_anno_pathways_from_file <- function(
   pw_name_col,        # flatfile colname: this column should contain pathway names
   export_raw_db       # OPTIONAL. Export the pathway database to a directory. Must be a string containing the path name with a .xlsx extension.
 ) {
-
+  
   # check arguments
   stopifnot("SummarizedExperiment" %in% class(D))
-
+  
   if (missing(flatfile))
     stop("flatfile must be given to fetch pathway annotation file from")
-
+  
   if (!file.exists(flatfile))
     stop(glue::glue("{flatfile} does not exist. input a valid flatfile path."))
-
+  
   if(!in_col %in% names(rowData(D)))
     stop(glue::glue("in_col is invalid. please enter an existing column name."))
-
-
+  
+  
   mti_logstatus(glue::glue("reading annotation file: {basename(flatfile)}, sheet: {sheet}"))
   # check flatfile colnames
   pwdb <- read_excel(path=flatfile, sheet=sheet)
-
+  
   flatfile_cols <- c(met_ID_col, pw_ID_col, pw_name_col)
   valid_names <- flatfile_cols %in% names(pwdb)
   if (!all(valid_names)) {
     invalid_names <- flatfile_cols[!valid_names]
-    stop(sprintf("Non-existent flatfile column names. Please replace: %s \n with one of: %s",
+    stop(sprintf("Non-existent flatfile column names. Please replace: %s \n with one of: %s", 
                  str_c(invalid_names, collapse = ", "),
                  str_c(names(pwdb), collapse = ", ")))
   }
-
-  pwdb %<>%
-    dplyr::select(met_ID = !!met_ID_col,
+  
+  pwdb %<>% 
+    dplyr::select(met_ID = !!met_ID_col, 
                   ID = !!pw_ID_col,
                   pathway_name = !!pw_name_col)
-
+  
   # create a dataframe that enables the mapping between pathway
   # names and IDs. Included also are num_total, num_measured,
   # num_pw_total, and num_pw_measured (see below for further details)
   # in this dataframe map
-
-  # num_total - overall number of metabolites in entire database (this will
-  # be a redundant, repeating number, identical in every row… but it’s the
+  
+  # num_total - overall number of metabolites in entire database (this will 
+  # be a redundant, repeating number, identical in every row… but it’s the 
   # easiest way to store it right now)
-  # pwdb$accession is used here, since there is a many-to-one mapping
+  # pwdb$accession is used here, since there is a many-to-one mapping 
   # between HMDB_id (secondary accessions) and accession
-  num_total <-
-    pwdb$met_ID %>%
-    unique() %>%
+  num_total <- 
+    pwdb$met_ID %>% 
+    unique() %>% 
     length()
-
+  
   # num_measured - overall number of measured metabolites found in the DB
-  num_measured =
-    pwdb$met_ID %>%
+  num_measured = 
+    pwdb$met_ID %>% 
     # for this intersect, similar to the one in pwdb_summary (below),
     # it is assumed that the IDs in the dataset D is non-redundant.
     # else this number may not be accurate.
-    intersect(rowData(D)[[in_col]]) %>%
+    intersect(rowData(D)[[in_col]]) %>% 
     length()
-
+  
   mti_logstatus(glue::glue("summarizing pathway information"))
   pwdb_summary <- pwdb
   # using methods from data.table reduces runtime by almost 10x as compared
   # to dplyr
-  pwdb_summary <-
+  pwdb_summary <- 
     setDT(pwdb_summary)[, `:=`(
-
+      
       num_total = num_total,
       num_measured = num_measured,
-
-      # num_pw_total - the total number of metabolites in that pathway
+      
+      # num_pw_total - the total number of metabolites in that pathway 
       # here, accession is used.
       # (overall DB background)
-      num_pw_total = uniqueN(met_ID),
-
-      # num_pw_measured - the number of measured metabolites in that pathway
+      num_pw_total = uniqueN(met_ID), 
+      
+      # num_pw_measured - the number of measured metabolites in that pathway 
       # (for the M type of analysis on the actual measured background).
-      num_pw_measured =
+      num_pw_measured = 
         sum(met_ID %in% rowData(D)[[in_col]], na.rm = TRUE)
     ),
-    by = ID] %>%
+    by = ID] %>% 
     # some IDs might have more than two names, however, these will be discarded
     # for now
-    unique(by = c("ID")) %>%
-    subset(!is.na(ID),
-           select = -met_ID)
-
-
+    unique(by = c("ID")) %>% 
+    subset(!is.na(ID), 
+           select = -met_ID) 
+  
+  
   mti_logstatus(glue::glue("nesting pathway IDs"))
-  # nest all the pathway IDs given our lieblings input IDs
-  pwdb_reduced <-
+  # nest all the pathway IDs given our lieblings input IDs 
+  pwdb_reduced <- 
     pwdb %>%
-    group_by(met_ID) %>%
-    filter(!is.na(ID)) %>%
-    distinct(met_ID, ID) %>%
-    nest(ID, .key = ID) %>%
-    mutate(ID =
+    group_by(met_ID) %>% 
+    filter(!is.na(ID)) %>% 
+    distinct(met_ID, ID) %>% 
+    nest(ID, .key = ID) %>% 
+    mutate(ID = 
              ID %>%
-             unlist(recursive = FALSE) %>%
+             unlist(recursive = FALSE) %>% 
              as.list())
-
-
+  
+  
   # match the nested pathways to our lieblings IDs
-  match_idx <-
+  match_idx <- 
     match(rowData(D)[[in_col]],
           pwdb_reduced$met_ID)
-
+  
   pw_col <- pwdb_reduced$ID[match_idx]
-
+  
   # add the pathway IDs into D
   rowData(D)[[out_col]] <- pw_col
-
+  
   # add pathway map to the metadata of D
-  metadata(D)$pathways[[out_col]] <-
+  metadata(D)$pathways[[out_col]] <- 
     pwdb_summary
-
-
+  
+  
   if(!missing(export_raw_db)) {
     openxlsx::write.xlsx(pwdb, export_raw_db)
   }
-
-
+  
+  
   funargs <- mti_funargs()
-  metadata(D)$results %<>%
+  metadata(D)$results %<>% 
     mti_generate_result(
       funargs = funargs,
       logtxt = sprintf('added pathway annotations using %s', basename(flatfile))
     )
-
+  
   D
-<<<<<<< Updated upstream
 }
-=======
-}
-
-
-
-if (FALSE) {
-  # Example -----------------------------------------------------------------
-  mt_logging(console=T)
-  D_alone <-
-    mt_files_load_metabolon(codes.makepath("MT/sampledata.xlsx"), "OrigScale") %>%
-    mt_anno_pathways_from_file(
-      in_col = "HMDb_ID", out_col = "smp_db",
-      flatfile = codes.makepath("snippets/packages//metabotools_external/hmdb/hmdb_preprocessed_4.0.xlsx"),
-      sheet = "hmdb",
-      met_ID_col = "HMDB_id", pw_ID_col = "SMP", pw_name_col = "pathway_name"
-    )
-
-
-  D_alone <-
-    mt_files_load_metabolon(codes.makepath("packages/metabotools/sampledata.xlsx"), "OrigScale") %>%
-    mt_anno_pathways_from_file(
-      in_col = "HMDb_ID", out_col = "janpw",
-      flatfile = codes.makepath("packages/metabotools_external/hmdb/hmdb_preprocessed_4.0.csv"),
-      met_ID_col = "HMDB_id", pw_ID_col = "SMP", pw_name_col = "pathway_name"
-    )
-
-
-}
-
->>>>>>> Stashed changes
--- mt_internal_imputeKNN_multicore.R
+++ mt_internal_imputeKNN_multicore.R
@@ -2,11 +2,11 @@
 
 #' kNN multi-core internal method.
 #'
-#'
+#' 
 #' Default settings are from the winning approach of Kiki's paper paper.
 #' https://link.springer.com/article/10.1007%2Fs11306-018-1420-2
 #' Specifically "knn.obs.euc.sel"
-#'
+#' 
 #' This script creates a tmp folder where distance matrices and imputed
 #' values are stored
 #
@@ -21,9 +21,10 @@
 #' @examples
 #' # in the context of a SE pipeline
 #' ... %>% mt_pre_impute_knn() %>% ...    # standard call
-#'
+#' 
 #' @author Parviz Gomari
-#'
+#' 
+#' 
 
 
 library(tidyverse)
@@ -34,51 +35,51 @@ mt_internal_imputeKNN_multicore <- function(dat,
                                             K=5,
                                             mc_cores = 5,
                                             verbose = T) {
-
-
+  
+  
   datimp <- dat
   # select row_index of missing observation
-  incom_obs <- which(apply(dat,1,function(x) any(is.na(x))))
-
+  incom_obs <- which(apply(dat,1,function(x) any(is.na(x)))) 
+  
   # select column_index of missing observation
   incom_vars <- which(apply(dat,2,function(x) any(is.na(x)))) %>% names()
-
-
+  
+  
   if(verbose)message(paste0("Number of imcomplete observations: ", length(incom_obs)))
-
+  
   # calculate correlation matrix using pairwise complete observations pairwise.complete.obs
   Cor <- cor(dat,use="p")
-
-
+  
+  
   dist_path <- "tmp/distance_matrices/"
   dir.create(dist_path, recursive = TRUE)
-
+  
   # Precalculate values that will be reused in the mclapply below. Increases speed...
   scale_dat <- scale(dat)
   dist_scale_dat <- dist(scale_dat)
-
+  
   # Get a list of sample distance matrices based on each incom_vars and varsel.
   # Basically what this is doing is that, if an incom.var is missing, find
   # metabolites with highest correlation, and based on these metabolites
   # calculate which samples are close.
   if(verbose)message("creating distance matrices")
   junk_collector <- mclapply(incom_vars, function(j) {
-
+    
     # select variables (metabolites) with > cor.var.sel
     varsel <- which(abs(Cor[j,])>cor.var.sel)    # ist j selbst dabei, ist aber ok
-
-    # If length varsel > 10, take index of the 11 highest ranking variables
+    
+    # If length varsel > 10, take index of the 11 highest ranking variables 
     # (metabolites) correlating with j
     if(length(varsel)>10) varsel <- order(abs(Cor[j,]),decreasing=T)[1:11]
-
-    # If length varsel < 5, take index of the 6 highest ranking variables
+    
+    # If length varsel < 5, take index of the 6 highest ranking variables 
     # (metabolites) correlating with j
     if(length(varsel)<5) varsel <- order(abs(Cor[j,]),decreasing=T)[1:6]
-
+    
     # calculate distance matrix of each sample based on scaled varsel data (column-wise)
     # Convert to a full matrix
-    D2 <- as.matrix(dist(scale_dat[,varsel]),upper=T,diag=T)
-
+    D2 <- as.matrix(dist(scale_dat[,varsel]),upper=T,diag=T) 
+    
     # if D2 has missing values, replace them by distance matrix of whole dataframe,
     # scaled by length of varsel and ncol(dat)
     if(any(is.na(D2))) {
@@ -87,85 +88,85 @@ mt_internal_imputeKNN_multicore <- function(dat,
     diag(D2) <- NA
     saveRDS(D2, paste0(dist_path, j, ".rds"), compress = FALSE)
     NULL
-  },
-  mc.cleanup = TRUE,
-  mc.preschedule = TRUE,
+  }, 
+  mc.cleanup = TRUE, 
+  mc.preschedule = TRUE, 
   mc.cores = mc_cores)
-
-
-
+  
+  
+  
   impute_path <- "tmp/imputed_samples/"
   dir.create(impute_path, recursive = TRUE)
-
+  
   if(verbose)message("calculating imputation values")
-  junk_collector <-
-    mclapply(incom_obs,
+  junk_collector <- 
+    mclapply(incom_obs, 
              function(i) {
-
+               
                # for the observation/sample that has a missing metabolite,
                # select all metabolites that have values
                incomvars <-  dat[i, is.na(dat[i,])] %>% names()
-
+               
                dattmp <-  dat
-
+               
                # iterate over te missing metabolites
                for (j in incomvars) {
-
+                 
                  # select sample distance matrix calculated for missing metabolite
                  D2_path <- paste0(dist_path, j, ".rds")
-                 D2 <- readRDS(D2_path)
-
+                 D2 <- readRDS(D2_path)     
+                 
                  if(any(!is.na(D2[i,]))) {
-
+                   
                    # order samples by decreasing distance
                    KNNids <- order(D2[i,],na.last=NA)
-
+                   
                    # omit neighbors that also have the same metabolite j missing
                    KNNids_naomit <- KNNids[sapply(KNNids,function(ii) any(!is.na(dat[ii,j])))]
-
+                   
                  }  else KNNids  <- NULL
-
+                 
                  # Select at most 10 nearest neighbors which overlap with KNNids_naomit
                  if(!is.null(KNNids)) KNNids_sel <- intersect(KNNids[1:min(K,length(KNNids))],KNNids_naomit)
-
+                 
                  # If there really are no KNNids_sel, get at most floor(K/2) metbaolites from KNNids_naomit
-                 # OR if there < floor(K/2) KNNids_sel metabolites, get at most floor(K/2) metbaolites
+                 # OR if there < floor(K/2) KNNids_sel metabolites, get at most floor(K/2) metbaolites 
                  # from KNNids_naomit
-                 if(length(KNNids_sel)<1) KNNids_sel <- KNNids_naomit[1:min(floor(K/2),length(KNNids_naomit))] else
+                 if(length(KNNids_sel)<1) KNNids_sel <- KNNids_naomit[1:min(floor(K/2),length(KNNids_naomit))] else 
                    if(length(which(sapply(KNNids_sel,function(ii) !is.na(dat[ii,j])))) < floor(K/2) )  KNNids_sel <- KNNids_naomit[1:min(floor(K/2),length(KNNids_naomit))]
-
+                 
                  # calculate the imputation value
                  if(any(!is.na(D2[i,])) & length(KNNids)>=1) {
                    dattmp_sel <- dattmp[KNNids_sel,j]
                    dattmp[i,j] <- sum(dattmp_sel*exp(-D2[i,KNNids_sel]),na.rm=T)/sum(exp(-D2[i,KNNids_sel])[!is.na(dattmp_sel)],na.rm=T) }
                }
-
+               
                saveRDS(dattmp[i,], paste0(impute_path, i, ".rds"), compress = FALSE)
-
+               
                NULL
-             },
-             mc.cleanup = TRUE,
-             mc.preschedule = TRUE,
+             }, 
+             mc.cleanup = TRUE, 
+             mc.preschedule = TRUE, 
              mc.cores = mc_cores)
-
-
-
-
+  
+  
+  
+  
   # Fill in imputed values
-
+  
   for (i in incom_obs){
-
+    
     datimp[i,] <- readRDS(paste0(impute_path, i, ".rds"))
-
+    
   }
-
-
+  
+  
   # if there are still values missing, just assign mean value
   datimp <- apply(datimp,2, function(x) {
     if(any(is.na(x))) x[is.na(x)] <- mean(x,na.rm=T)
     x})
-
-
+  
+  
   datimp
 }
 
