9fdf32a8e885c5693c967813e2e3886fda3ca94b
KelseyChetnik <kelsey.chetnik@gmail.com>
2020-05-29 15:01:51 -0400

    Add post functions to MetaboTools

---
+++ MetaboTools/.Rproj.user/4B518C8D/sources/s-5A35E1DC/9680FC11-contents
@@ -0,0 +1,62 @@
+#' Multiple testing correction
+#'
+#' Adjust output of statistical tests for multiple testing
+#'
+#' @param D \code{SummarizedExperiment} input
+#' @param stat_name name of the statistical comparison to adjust
+#' @param p_col name of p-value column to adjust
+#' @param method which method to use for multiple testing (see p.adjust)
+#'
+#' @return $result: statistical object
+#'
+#' @examples
+#' \dontrun{# correct the statistical comparison called "Li's" using Benjamini-Hochberg
+#' ... %>%
+#'  mt_post_multTest(stat_name="Li's", method="BH") %>% ...
+#'  }
+#'
+#' @author JK, JZ
+#'
+#' @export
+mt_post_multTest <- function(D,
+                             stat_name,
+                             p_col = p.value,
+                             method = "bonferroni"){
+    p_col <- enquo(p_col)
+
+    ## stat
+    if(missing(stat_name))
+        stop("stat_name must be given")
+
+    ## FIND ENTRY
+    stat_id <- metadata(D)$results %>%
+                         map_lgl(~"stats" %in% .x$fun && .x$output$name == stat_name) %>%
+                         which()
+    if(length(stat_id) == 0)
+        stop("stat element with name ", stat_name, " does not exist")
+    if(length(stat_id)  > 1)
+        stop("there are multiple stat elements with name ", stat_name)
+
+    ## DO CORRECTION
+    metadata(D)$results[[stat_id]]$output$table %<>%
+                  dplyr::mutate(p.adj = p.adjust(!!p_col, method = method))
+
+
+    # ## UPDATE LOG ENTRIES
+    # metadata(D)$results[[stat_id]]$logtxt %<>%
+    #               str_c(., ", multiple testing adjusted (", method, ")")
+    # metadata(D)$results[[stat_id]]$logshort %<>%
+    #               str_c(., " [", method, "]")
+
+    ## add status information & plot
+    funargs <- mti_funargs()
+    metadata(D)$results %<>%
+                  mti_generate_result(
+                      funargs = funargs,
+                      logtxt = sprintf("Multiple testing correction of '%s' using '%s'", stat_name, method),
+                      output = NULL
+                  )
+    ## RETURN
+    D
+
+}
--- MetaboTools/R/mt_post_addFC.R
+++ MetaboTools/R/mt_post_addFC.R
@@ -1,47 +1,46 @@
-
 #' Compute fold-change
 #'
 #' Add metabolite fold-changes to results table
 #'
 #' @param D \code{SummarizedExperiment} input
-#' @param statname name of the statistical comparison
+#' @param stat_name name of the statistical comparison
 #' @param correct_confounder confounders to adjust for before calculating fold-changes
 #' @param combine function with respect to which fold changes are computed (e.g. difference of the log means or median)
-#' 
+#'
 #' @return $result: statistical object
-#' 
-#' @example
-#' # add fold-changes to the result table of the statistical comparison called "comparison1", after correcting for variable "age"
+#'
+#' @examples
+#' \dontrun{# add fold-changes to the result table of the statistical comparison called "comparison1", after correcting for variable "age"
 #' ... %>%
-#'  mt_post_addFC(statname="comparison1", correct_confounder=age) %>% ...
-#' 
-#' 
+#'  mt_post_addFC(stat_name="comparison1", correct_confounder=age) %>% ...
+#'  }
+#'
+#'
 #' @author JZ
-#' 
+#'
 #' @export
 mt_post_addFC <- function(D,
-                          statname,
+                          stat_name,
                           correct_confounder,
-                          combine = function(x){mean(x,na.rm=T)},
-                          ...){
-  
+                          combine = function(x){mean(x,na.rm=T)}){
+
   ## FOLDCHANGE FUNCTION (CONSIDER PREVIOUS LOG)
   if ((length(mti_res_get_path(D, c("pre","trans","log"))) != 1) &&
       (length(mti_res_get_path(D, c("flag","logged"))) != 1))
     stop("fold-changes can only be calculated for log-scale data")
-  
+
   ## stat
-  if(missing(statname))
-    stop("statname must be given")
+  if(missing(stat_name))
+    stop("stat_name must be given")
   ## FIND ENTRY
   stat_id <- metadata(D)$results %>%
-    map_lgl(~"stats" %in% .x$fun && .x$output$name == statname) %>%
+    map_lgl(~"stats" %in% .x$fun && .x$output$name == stat_name) %>%
     which()
   if(length(stat_id) == 0)
-    stop("stat element with name ", statname, " does not exist")
+    stop("stat element with name ", stat_name, " does not exist")
   if(length(stat_id)  > 1)
-    stop("there are multiple stat elements with name ", statname)
-  
+    stop("there are multiple stat elements with name ", stat_name)
+
   ## stop if the results are coming from an ANOVA, determine from first model
   if (class( metadata(D)$results[[ stat_id ]] %>%
              .$output %>%
@@ -50,7 +49,7 @@ mt_post_addFC <- function(D,
              .[[1]])[[1]] == "anova") {
     stop("Cannot add fold change for ANOVA analysis.")
   }
-  
+
   ## GET OUTCOME
   model <- metadata(D)$results[[ stat_id ]] %>%
     .$output %>%
@@ -64,7 +63,7 @@ mt_post_addFC <- function(D,
     ## select first phenotype = outcome of interest
     .[,1, drop = F]
   outcome    <- colnames(model)[[1]]
-  
+
   ## CORRECT FOR CONFOUNDER
   formula    <- metadata(D)$results[[ stat_id ]]$output$formula
   confounder <- update.formula(formula, str_c(".~.-", outcome))
@@ -73,24 +72,24 @@ mt_post_addFC <- function(D,
     mti_logstatus(glue::glue("correcting for {confounder}"))
     D <- mti_correctConfounder(D, confounder)
   }
-  
+
   ## EXTRACT DATA
   if(is.factor(colData(D)[[outcome]]))model[[outcome]] <- as.factor(model[[outcome]])
   # d_fc <- mti_format_se_samplewise(D) %>%
   #     ## use only levels that were used in stat
   #     inner_join(model, by = outcome)
-  d_fc <- mti_format_se_samplewise(D) 
+  d_fc <- mti_format_se_samplewise(D)
   keep <- d_fc[[outcome]] %in% model[[outcome]]
   d_fc <- d_fc[keep,]
   d_fc[[outcome]] <- as.factor(d_fc[[outcome]])
   d_fc[[outcome]] <- droplevels(d_fc[[outcome]])
-  
+
   ## CHECK TYPE OF OUTCOME
   if(!(class(d_fc[[ outcome ]]) %in% c("factor", "character")))
     stop(glue::glue("Fold-changes are only meaningful for factor/character, but {outcome} is a {class(d_fc[[outcome]])}"))
   if(length(unique(d_fc[[ outcome ]])) != 2)
     stop(glue::glue("Fold-changes are only meaningful for 2 groups, but {outcome} has {length(unique(d_fc[[outcome]]))}"))
-  
+
   ## GET LEVELS IN ORDER OF FACTOR LEVELS
   outcome_q <- rlang::sym(outcome)
   if( "factor" %in% class(d_fc[[outcome]])){
@@ -110,23 +109,23 @@ mt_post_addFC <- function(D,
     tidyr::spread(!!outcome_q, value) %>%
     dplyr::mutate(fc = !!levels_1 - !!levels_2) %>%
     dplyr::select(var, fc)
-  
+
   ## ADD TO RESULTS
   metadata(D)$results[[stat_id]]$output$table %<>%
     left_join(d_fc, by = "var")
-  
+
   ## make sure fold change has the same sign as statistic
   ## this is a debug solution, it should come out properly from the code above... but this fixes the bug for now
   metadata(D)$results[[stat_id]]$output$table$fc <-
     metadata(D)$results[[stat_id]]$output$table$fc %>% abs() * sign(metadata(D)$results[[stat_id]]$output$table$statistic)
-  
-  
+
+
   ## add status information & plot
   funargs <- mti_funargs()
-  metadata(D)$results %<>% 
+  metadata(D)$results %<>%
     mti_generate_result(
       funargs = funargs,
-      logtxt = sprintf("Calculated foldchanges for %s", statname),
+      logtxt = sprintf("Calculated foldchanges for %s", stat_name),
       output = NULL
     )
   ## RETURN
