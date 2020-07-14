a98d501b1935e5a7b2986c34d69e36dc93b374e1
KelseyChetnik <kelsey.chetnik@gmail.com>
2020-07-14 10:26:32 -0400

    Fix order of mt_files_load_olink functions

--- MetaboTools/R/mt_files_load_olink.R
+++ MetaboTools/R/mt_files_load_olink.R
@@ -27,6 +27,67 @@
 #'
 #' @export
 ###########################################################################
+mt_files_load_olink <- function(
+  file           # Olink Metabolon xlsx file
+) {
+
+  result=list()
+  result$info = "nothing yet"
+
+  # read the Olink file
+  odata = read_NPX(file)
+
+  # NPX values are ~ PCA cycles, hence on some kind of a log-scale
+  # to be in-line with meta-tools, un-logscale the data using 2 as a basis
+  odata$NPX = 2^odata$NPX
+
+  # convert to wide format
+  wdata = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "NPX")
+  xdata = simplify2array(wdata[,-1])
+  rownames(xdata) = wdata$SampleID
+
+  # convert to SummarizedExperiment
+  # the assay is called "exprs" to be compatible with autonomics
+  D = SummarizedExperiment(assay = list(exprs = t(xdata)),
+                           colData = DataFrame(sample_id = rownames(xdata)),
+                           rowData = DataFrame(feature_id = colnames(xdata),
+                                               LODdata = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "LOD")[1,-1] %>% unlist() %>% as.numeric() %>% unname(),
+                                               MissingFreq = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "MissingFreq")[1,-1] %>% unlist() %>% as.numeric() %>% unname(),
+                                               OlinkID = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "OlinkID")[1,-1] %>% unlist() %>% unname(),
+                                               UniProt = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "UniProt")[1,-1] %>% unlist() %>% unname(),
+                                               Assay = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "Assay")[1,-1] %>% unlist() %>% unname(),
+                                               Panel = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "Panel")[1,-1] %>% unlist() %>% unname(),
+                                               Panel_Version = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "Panel_Version")[1,-1] %>% unlist() %>% unname(),
+                                               PlateID = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "PlateID")[1,-1] %>% unlist() %>% unname()
+                           ),
+                           metadata = list(sessionInfo=sessionInfo(), parseInfo=result$info)
+  )
+
+  # do some sanity checks
+  stopifnot (length(which(colnames(xdata) != rowData(D)$OlinkID)) == 0)
+  MissingFreq2 = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "MissingFreq")[2,-1] %>% unlist() %>% as.numeric() %>% unname()
+  stopifnot (length(which(MissingFreq2 != rowData(D)$MissingFreq)) == 0)
+
+  # as column names, use "BIOCHEMICAL" for autonomics and "name" for MetaboTools
+  rowData(D)$BIOCHEMICAL = rowData(D)$Assay
+  rowData(D)$name = rowData(D)$Assay
+
+  # as row names, use "SAMPLE_NAME", if available
+  D$SAMPLE_NAME = D$sample_id
+
+  # add status information
+  funargs <- mti_funargs()
+  metadata(D)$results %<>%
+    mti_generate_result(
+      funargs = funargs,
+      logtxt = sprintf("loaded Olink file: %s", basename(file))
+    )
+
+  # return
+  D
+
+}
+###########################################################################
 read_NPX <- function (filename)
 {
   NORM_FLAG <- F
@@ -191,65 +252,4 @@ read_NPX <- function (filename)
       matches("*Inc Ctrl*"),
       matches("*Det Ctrl*")
     )
-}
-###########################################################################
-mt_files_load_olink <- function(
-  file           # Olink Metabolon xlsx file
-) {
-  
-  result=list()
-  result$info = "nothing yet"
-  
-  # read the Olink file
-  odata = read_NPX(file)
-  
-  # NPX values are ~ PCA cycles, hence on some kind of a log-scale
-  # to be in-line with meta-tools, un-logscale the data using 2 as a basis
-  odata$NPX = 2^odata$NPX
-  
-  # convert to wide format
-  wdata = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "NPX")
-  xdata = simplify2array(wdata[,-1])
-  rownames(xdata) = wdata$SampleID
-  
-  # convert to SummarizedExperiment
-  # the assay is called "exprs" to be compatible with autonomics
-  D = SummarizedExperiment(assay = list(exprs = t(xdata)),
-                           colData = DataFrame(sample_id = rownames(xdata)),
-                           rowData = DataFrame(feature_id = colnames(xdata),
-                                               LODdata = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "LOD")[1,-1] %>% unlist() %>% as.numeric() %>% unname(),
-                                               MissingFreq = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "MissingFreq")[1,-1] %>% unlist() %>% as.numeric() %>% unname(),
-                                               OlinkID = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "OlinkID")[1,-1] %>% unlist() %>% unname(),
-                                               UniProt = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "UniProt")[1,-1] %>% unlist() %>% unname(),
-                                               Assay = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "Assay")[1,-1] %>% unlist() %>% unname(),
-                                               Panel = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "Panel")[1,-1] %>% unlist() %>% unname(),
-                                               Panel_Version = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "Panel_Version")[1,-1] %>% unlist() %>% unname(),
-                                               PlateID = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "PlateID")[1,-1] %>% unlist() %>% unname()
-                           ),
-                           metadata = list(sessionInfo=sessionInfo(), parseInfo=result$info)
-  )
-  
-  # do some sanity checks
-  stopifnot (length(which(colnames(xdata) != rowData(D)$OlinkID)) == 0)
-  MissingFreq2 = reshape2::dcast(odata,formula = " SampleID ~ OlinkID", value.var = "MissingFreq")[2,-1] %>% unlist() %>% as.numeric() %>% unname()
-  stopifnot (length(which(MissingFreq2 != rowData(D)$MissingFreq)) == 0)
-  
-  # as column names, use "BIOCHEMICAL" for autonomics and "name" for MetaboTools
-  rowData(D)$BIOCHEMICAL = rowData(D)$Assay
-  rowData(D)$name = rowData(D)$Assay
-  
-  # as row names, use "SAMPLE_NAME", if available
-  D$SAMPLE_NAME = D$sample_id
-  
-  # add status information
-  funargs <- mti_funargs()
-  metadata(D)$results %<>%
-    mti_generate_result(
-      funargs = funargs,
-      logtxt = sprintf("loaded Olink file: %s", basename(file))
-    )
-  
-  # return
-  D
-  
 }
